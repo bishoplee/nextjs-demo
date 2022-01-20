726e2437155be02dd18fd059a755687bfa051a91
Olaleye Oladipo <olaleye@thebluematrix.com>
2022-01-20 23:17:42 +0100

    added about-us page

--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,14 @@
-//import Head from 'next/head'
+import Head from 'next/head'
 //import Image from 'next/image'
 //import styles from '../styles/Home.module.css'
 
 export default function Home() {
   return (
     <div>
+      <Head>
+        <title>Demo NextJS Website</title>
+        <meta name='keywords' content='nextjs, react, reactjs, web development, software programming' />
+      </Head>
       <h1>Welcome</h1>
     </div>
   )
